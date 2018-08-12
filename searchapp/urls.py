5a05834bbebc5e161d5c2a5fbcfcbaece2f2eeb9
ishanisri <ishasri.1212@gmail.com>
2018-08-12 19:08:55 +0530

    added to db, bugs not checked yet

--- searchapp/urls.py
+++ searchapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = [
-    path('',views.student_view,name="student_view"),
+app_name='searchapp'
+urlpatterns=[
+   path('',views.upload_view,name='upload'),
+  path('upload',views.add_questions,name='add_questions'),
+
 ]
--- workgen/urls.py
+++ workgen/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('searchapp.urls')),
+   path('upload/',include('searchapp.urls')),
 ]
