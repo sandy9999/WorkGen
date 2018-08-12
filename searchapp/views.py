aa13aa86fe5df97e0ccb795793c4affd32f4f55f
sandy9999 <luckysandy99@gmail.com>
2018-08-12 17:04:49 +0530

    Made chapter options dynamic

--- searchapp/views.py
+++ searchapp/views.py
@@ -6,5 +6,5 @@ from .models import Mentor,Questions,MCQOptions
 def student_view(request):
     subject = Questions.objects.order_by('subject').values('subject').distinct()
     chap = Questions.objects.filter(subject='Science')
-    template_data = {'subject':subject,'chap':chap,}
+    template_data = {'subject':subject,'chap':chap}
     return render(request,'student_view.html',template_data)
