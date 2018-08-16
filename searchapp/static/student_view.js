0438210a46ee5c0aec64de884f6d363764ed1b11
sandy9999 <luckysandy99@gmail.com>
2018-08-17 01:49:39 +0530

    Number input to be spaced

--- searchapp/static/student_view.js
+++ searchapp/static/student_view.js
@@ -58,18 +58,26 @@ $(document).ready(function(){
 	showOnFocus: false,
 	on: "click"
 });
-	$('#subject').dropdown({
-		onChange: function (value, text, $selectedItem) {
-		if(value == "Science")
-		{
-			$('#chapter .menu div').remove();
-			$('<div class="item" data-value="Motion">Motion</div>').appendTo('#chapter .menu');
+
+$('#subjectTest').dropdown({
+	onChange: function (value, text, $selectedItem) {
+		if(value == "Science") {
+			$('#chapterTest .menu div').remove();
+			$('<div class="item" data-value="Motion">Motion</div>').appendTo('#chapterTest .menu');
 		}
-		else if(value == "Math")
-		{
-			$('#chapter div div').remove();
-			$('<div class="item" data-value="Rational Nos">Rational Nos</div>').appendTo('#chapter .menu');
+		else if(value == "Math") {
+			$('#chapterTest div div').remove();
+			$('<div class="item" data-value="Rational Nos">Rational Nos</div>').appendTo('#chapterTest .menu');
 		}
+		},
+		forceSelection: false,
+		selectOnKeydown: false,
+		showOnFocus: false,
+		on: "click"
+	});
+
+$('#chapterTest').dropdown({
+	onChange: function (value, text, $selectedItem) {
 	},
 	forceSelection: false,
 	selectOnKeydown: false,
@@ -77,7 +85,25 @@ $(document).ready(function(){
 	on: "click"
 
 });
-	$('#chapter').dropdown({
+
+$('#subjectGeneric').dropdown({
+	onChange: function (value, text, $selectedItem) {
+		if(value == "Science") {
+			$('#chapterGeneric .menu div').remove();
+			$('<div class="item" data-value="Motion">Motion</div>').appendTo('#chapterGeneric .menu');
+		}
+		else if(value == "Math") {
+			$('#chapterGeneric div div').remove();
+			$('<div class="item" data-value="Rational Nos">Rational Nos</div>').appendTo('#chapterGeneric .menu');
+		}
+		},
+		forceSelection: false,
+		selectOnKeydown: false,
+		showOnFocus: false,
+		on: "click"
+});
+
+$('#chapterGeneric').dropdown({
 	onChange: function (value, text, $selectedItem) {
 	},
 	forceSelection: false,
@@ -85,7 +111,25 @@ $(document).ready(function(){
 	showOnFocus: false,
 	on: "click"
 });
-	$('#qtype').dropdown({
+
+$('#subjectCustomized').dropdown({
+	onChange: function (value, text, $selectedItem) {
+		if(value == "Science") {
+			$('#chapterCustomized .menu div').remove();
+			$('<div class="item" data-value="Motion">Motion</div>').appendTo('#chapterCustomized .menu');
+		}
+		else if(value == "Math") {
+			$('#chapterCustomized div div').remove();
+			$('<div class="item" data-value="Rational Nos">Rational Nos</div>').appendTo('#chapterCustomized .menu');
+		}
+		},
+		forceSelection: false,
+		selectOnKeydown: false,
+		showOnFocus: false,
+		on: "click"
+});
+
+$('#chapterCustomized').dropdown({
 	onChange: function (value, text, $selectedItem) {
 	},
 	forceSelection: false,
@@ -93,7 +137,8 @@ $(document).ready(function(){
 	showOnFocus: false,
 	on: "click"
 });
-	$('#stud_name').dropdown({
+
+$('#qtypeTest').dropdown({
 	onChange: function (value, text, $selectedItem) {
 	},
 	forceSelection: false,
@@ -101,4 +146,41 @@ $(document).ready(function(){
 	showOnFocus: false,
 	on: "click"
 });
+
+$('#qtypeGeneric').dropdown({
+	onChange: function (value, text, $selectedItem) {
+	},
+	forceSelection: false,
+	selectOnKeydown: false,
+	showOnFocus: false,
+	on: "click"
+});
+
+$('#qtypeCustomized').dropdown({
+	onChange: function (value, text, $selectedItem) {
+	},
+	forceSelection: false,
+	selectOnKeydown: false,
+	showOnFocus: false,
+	on: "click"
+});
+
+$('#stud_name').dropdown({
+	onChange: function (value, text, $selectedItem) {
+	},
+	forceSelection: false,
+	selectOnKeydown: false,
+	showOnFocus: false,
+	on: "click"
+
+});
+	});
+$('#random-setting').dropdown({
+        onChange: function (value, text, $selectedItem) {
+        },
+        forceSelection: false,
+        selectOnKeydown: false,
+        showOnFocus: false,
+        on: "click"
+        });
 });
