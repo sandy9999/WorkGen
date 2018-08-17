7cfc737af6b39639e07adc4dfe40f1450892bfef
ishanisri <ishasri.1212@gmail.com>
2018-08-17 15:14:14 +0530

    fixed small bugs

--- searchapp/urls.py
+++ searchapp/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name='searchapp'
 urlpatterns=[
-   path('',views.upload_view,name='upload'),
-  path('upload',views.add_questions,name='add_questions'),
+   path('questions_upload',views.add_questions_view,name='questions_upload'),
+   path('upload',views.add_questions,name='upload'),
+   path('login',views.login_view,name='login'),
+   path('logout',views.logout_view,name='logout'),
+   path('mentor_view',views.mentor_view,name='mentor_view')
 
 ]
