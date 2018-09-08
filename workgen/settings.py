b675fb6fce36347a8d3b914fd424fa287af6137b
paiakshay998 <paiakshay998@gmail.com>
2018-09-08 10:39:18 +0530

    Add CORS whitelist

--- workgen/settings.py
+++ workgen/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'searchapp.apps.SearchappConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -164,3 +166,7 @@ LOGGING = {
         }
     }
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8000',
+)
