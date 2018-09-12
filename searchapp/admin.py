4251390480164133cbed937a51020dfacc2ab1b2
paiakshay998 <paiakshay998@gmail.com>
2018-09-12 18:18:42 +0530

    Add models to admin dashboard

--- searchapp/admin.py
+++ searchapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from searchapp.models import Mentor, Subject, GeneratedQuestionPaper
+from searchapp.models import *
 # Register your models here.
 
 admin.site.register(Mentor)
 admin.site.register(Subject)
 admin.site.register(GeneratedQuestionPaper)
+admin.site.register(Questions)
+admin.site.register(SubjectSplit)
