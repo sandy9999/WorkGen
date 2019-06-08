751dc5e79a7c2b0482a339e97d4269b5f669b9c4
sandy9999 <luckysandy99@gmail.com>
2019-06-08 19:56:57 +0530

    Separate view to add/delete chapters and subject splits

--- searchapp/urls.py
+++ searchapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name='searchapp'
 urlpatterns=[
+   
    path('questions_upload',views.add_questions_view,name='questions_upload'),
    path('generated_documents', views.generated_documents_view, name='generated_documents'),
    path('download_customized_docx', views.download_customized_docx, name='download_customized_docx'),
@@ -12,7 +13,12 @@ urlpatterns=[
    path('logout',views.logout_view,name='logout'),
    path('mentor_view',views.mentor_view,name='mentor_view'),
    path('student_view', views.student_view, name='student_view'),
+   path('chapter_and_split_view',views.chapter_and_split_view,name='chapter_and_split_view'),
    path('get_chapters', views.get_chapters, name='get_chapters'),
+   path('add_chapter', views.add_chapter, name='add_chapter'),
+   path('delete_chapters', views.delete_chapters, name='delete_chapters'),
+   path('delete_subject_split', views.delete_subject_split, name='delete_subject_split'),
+   path('add_subject_split', views.add_subject_split, name='add_subject_split'),
    path('get_test_paper', views.get_test_paper, name='get_test_paper'),
    path('get_generic_paper', views.get_generic_paper, name='get_generic_paper'),
    path('get_test_format', views.get_test_format, name='get_test_format'),
