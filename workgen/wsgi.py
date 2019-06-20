7cd11f32a3f35206275b2ca5e66c697d3797184c
sandy9999 <luckysandy99@gmail.com>
2019-06-20 13:42:19 +0530

    Changed around some http links to https

--- workgen/wsgi.py
+++ workgen/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workgen.settings')
+os.environ['HTTPS'] = "on"
 
 application = get_wsgi_application()
