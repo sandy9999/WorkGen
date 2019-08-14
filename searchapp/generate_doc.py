d57ab029298eb589788aac05f2340aff628f107f
Sandhya Saravanan <luckysandy99@gmail.com>
2019-08-14 10:11:39 +0530

    Fixed broken master (#30)
    * Fixed broken master
    
    * refactor: document generator script
    
    * refactor: test_paper.py
    
    * refactor2: document generator script (variable names)
    
    * Render template according to mode
    
    * Convert to https

--- searchapp/generate_doc.py
+++ searchapp/generate_doc.py
@@ -2,272 +2,70 @@ from docx import Document
 import datetime
 import os
 
+qtype_to_section = {"1A": "A", "1B": "B", "2": "C", "3": "D", "5": "E"}
+
+def get_no_of_questions(questions_mapping, question_type):
+    count = 0
+    for row in questions_mapping:
+        if row.get('question_type') == question_type:
+            for question in row['question']:
+                count = count+1
+    return count
+
+def add_headings(document, subject):
+    heading = document.add_heading("EXAM")
+    heading.alignment = 1
+    subject_heading = document.add_heading(subject,level = 2)
+    subject_heading.alignment = 1
+    para = document.add_paragraph()
+    run = para.add_run()
+    run.add_break()
+
+def add_questions(document, questions_mapping, question_type, question_no, no_of_questions):
+    section=document.add_paragraph()
+    section.add_run("Section " + qtype_to_section[question_type]).bold=True
+    section.alignment = 1
+    subsection = document.add_paragraph()
+    subsection.add_run(str(no_of_questions) + " questions of " + question_type[0] + " marks each")
+    subsection.alignment = 1
+    for row in questions_mapping:
+        if row.get('question_type')== question_type:
+            p = document.add_paragraph()
+            p.add_run("Attempt only "+str(min(row['attempt'],no_of_questions))+" questions").italic=True
+            p.alignment = 1
+            for question in row['question']:
+                question_no = question_no+1
+                p1 = document.add_paragraph(str(question_no)+": ")
+                p1.add_run(question)
+    para = document.add_paragraph()
+    run = para.add_run()
+    run.add_break()
+    return question_no
+
 def convert_customized_to_doc(questions_mapping, subject):
     document = Document()
     for name in questions_mapping:
-        pp = document.add_paragraph()
-        stud_name = pp.add_run("Name: " + name)
-        stud_name.add_break()
-        heading = document.add_heading("EXAM")
-        heading.alignment = 1
-        heading2 = document.add_heading(subject,level = 2)
-        heading2.alignment = 1
         para = document.add_paragraph()
-        run = para.add_run()
-        run.add_break()
-        i = 0
-        ct = 0
-        for row in questions_mapping[name]:
-            if row.get('question_type')=='1A':
-                for question in row['question']:
-                    ct = ct+1
-        section=document.add_paragraph()
-        section.add_run("Section A").bold=True
-        section.alignment = 1
-        subsection = document.add_paragraph()
-        subsection.add_run(str(ct) + " questions of 1 marks each")
-        subsection.alignment = 1
-        for row in questions_mapping[name]:
-            if row.get('question_type')=='1A':
-                p = document.add_paragraph()
-                p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-                p.alignment = 1
-                for question in row['question']:
-                    i = i+1
-                    p1 = document.add_paragraph(str(i)+": ")
-                    p1.add_run(question)
-
-        para = document.add_paragraph()
-        run = para.add_run()
-        run.add_break()
-        ct = 0
-        for row in questions_mapping[name]:
-            if row.get('question_type')=='1B':
-                for question in row['question']:
-                    ct = ct+1
-        section=document.add_paragraph()
-        section.add_run("Section B").bold=True
-        section.alignment = 1
-        subsection = document.add_paragraph()
-        subsection.add_run(str(ct) + " questions of 1 marks each")
-        subsection.alignment = 1
-        for row in questions_mapping[name]:
-            if row['question_type'] == '1B':
-                p = document.add_paragraph()
-                p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-                p.alignment = 1
-                for question in row['question']:
-                    i=i+1
-                    p1=document.add_paragraph(str(i)+": ")
-                    p1.add_run(question)
-        
-        para = document.add_paragraph()
-        run = para.add_run()
-        run.add_break()
-        ct = 0
-        for row in questions_mapping[name]:
-            if row.get('question_type')=='2':
-                for question in row['question']:
-                    ct = ct+1
-        section=document.add_paragraph()
-        section.add_run("Section C").bold=True
-        section.alignment = 1
-        subsection = document.add_paragraph()
-        subsection.add_run(str(ct) + " questions of 2 marks each")
-        subsection.alignment = 1
-        for row in questions_mapping[name]:
-            if row['question_type'] == '2':
-                p=document.add_paragraph()
-                p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-                p.alignment = 1
-                for question in row['question']:
-                    i=i+1
-                    p1=document.add_paragraph(str(i)+": ")
-                    p1.add_run(question)
-        
-        para = document.add_paragraph()
-        run = para.add_run()
-        run.add_break()
-        ct = 0
-        for row in questions_mapping[name]:
-            if row.get('question_type')=='3':
-                for question in row['question']:
-                    ct = ct+1
-        section=document.add_paragraph()
-        section.add_run("Section D").bold=True
-        section.alignment = 1
-        subsection = document.add_paragraph()
-        subsection.add_run(str(ct) + " questions of 3 marks each")
-        subsection.alignment = 1
-        for row in questions_mapping[name]:
-            if row['question_type'] == '3':
-                p=document.add_paragraph()
-                p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-                p.alignment = 1
-                for question in row['question']:
-                    i = i+1
-                    p1 = document.add_paragraph(str(i)+" : ")
-                    p1.add_run(question)
-
-        para = document.add_paragraph()
-        run = para.add_run()
-        run.add_break()
-        ct = 0
-        for row in questions_mapping[name]:
-            if row.get('question_type')=='5':
-                for question in row['question']:
-                    ct = ct+1
-        section=document.add_paragraph()
-        section.add_run("Section E").bold=True
-        section.alignment = 1
-        subsection = document.add_paragraph()
-        subsection.add_run(str(ct) + " questions of 5 marks each")
-        subsection.alignment = 1
-        for row in questions_mapping[name]:
-            if row['question_type'] == '5':
-                p = document.add_paragraph()
-                p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-                p.alignment = 1
-                for question in row['question']:
-                    i = i+1
-                    p1 = document.add_paragraph(str(i)+" : ")
-                    p1.add_run(question)
+        student_name = para.add_run("Name: " + name)
+        student_name.add_break()
+        add_headings(document, subject)
+        question_no = 0
+        for qtype in qtype_to_section:
+            no_of_questions = get_no_of_questions(questions_mapping[name], qtype)
+            question_no = add_questions(document, questions_mapping[name], qtype, question_no, no_of_questions)
         document.add_page_break()
-
     filename = "question_worksheet_" + datetime.datetime.now().__str__() + ".docx"
     filepath = os.path.join('searchapp/static/docs', filename)
     document.save(filepath)
     return filepath
 
-
 def convert_to_doc(questions_mapping, subject):
     document = Document()
-    heading = document.add_heading("EXAM")
-    heading.alignment = 1
-    heading2 = document.add_heading(subject,level = 2)
-    heading2.alignment = 1
-    para = document.add_paragraph()
-    run = para.add_run()
-    run.add_break()
-    i = 0
-
-    ct = 0
-    for row in questions_mapping:
-        if row.get('question_type')=='1A':
-            for question in row['question']:
-                ct = ct+1
-    section=document.add_paragraph()
-    section.add_run("Section A").bold=True
-    section.alignment = 1
-    subsection = document.add_paragraph()
-    subsection.add_run(str(ct) + " questions of 1 marks each")
-    subsection.alignment = 1
-    for row in questions_mapping:
-        if row.get('question_type')=='1A':
-            p = document.add_paragraph()
-            p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-            p.alignment = 1
-            for question in row['question']:
-                i = i+1
-                p1 = document.add_paragraph(str(i)+": ")
-                p1.add_run(question)
-
-    para = document.add_paragraph()
-    run = para.add_run()
-    run.add_break()
-    ct = 0
-    for row in questions_mapping:
-        if row.get('question_type')=='1B':
-            for question in row['question']:
-                ct = ct+1
-    section=document.add_paragraph()
-    section.add_run("Section B").bold=True
-    section.alignment = 1
-    subsection = document.add_paragraph()
-    subsection.add_run(str(ct) + " questions of 1 marks each")
-    subsection.alignment = 1
-    for row in questions_mapping:
-        if row['question_type'] == '1B':
-            p = document.add_paragraph()
-            p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-            p.alignment = 1
-            for question in row['question']:
-                i=i+1
-                p1=document.add_paragraph(str(i)+": ")
-                p1.add_run(question)
-    
-    para = document.add_paragraph()
-    run = para.add_run()
-    run.add_break()
-    ct = 0
-    for row in questions_mapping:
-        if row.get('question_type')=='2':
-            for question in row['question']:
-                ct = ct+1
-    section=document.add_paragraph()
-    section.add_run("Section C").bold=True
-    section.alignment = 1
-    subsection = document.add_paragraph()
-    subsection.add_run(str(ct) + " questions of 2 marks each")
-    subsection.alignment = 1
-    for row in questions_mapping:
-        if row['question_type'] == '2':
-            p=document.add_paragraph()
-            p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-            p.alignment = 1
-            for question in row['question']:
-                i=i+1
-                p1=document.add_paragraph(str(i)+": ")
-                p1.add_run(question)
-    
-    para = document.add_paragraph()
-    run = para.add_run()
-    run.add_break()
-    ct = 0
-    for row in questions_mapping:
-        if row.get('question_type')=='3':
-            for question in row['question']:
-                ct = ct+1
-    section=document.add_paragraph()
-    section.add_run("Section D").bold=True
-    section.alignment = 1
-    subsection = document.add_paragraph()
-    subsection.add_run(str(ct) + " questions of 3 marks each")
-    subsection.alignment = 1
-    for row in questions_mapping:
-        if row['question_type'] == '3':
-            p=document.add_paragraph()
-            p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-            p.alignment = 1
-            for question in row['question']:
-                i = i+1
-                p1 = document.add_paragraph(str(i)+" : ")
-                p1.add_run(question)
-
-    para = document.add_paragraph()
-    run = para.add_run()
-    run.add_break()
-    ct = 0
-    for row in questions_mapping:
-        if row.get('question_type')=='5':
-            for question in row['question']:
-                ct = ct+1
-    section=document.add_paragraph()
-    section.add_run("Section E").bold=True
-    section.alignment = 1
-    subsection = document.add_paragraph()
-    subsection.add_run(str(ct) + " questions of 5 marks each")
-    subsection.alignment = 1
-    for row in questions_mapping:
-        if row['question_type'] == '5':
-            p = document.add_paragraph()
-            p.add_run("Attempt only "+str(min(row['attempt'],ct))+" questions").italic=True
-            p.alignment = 1
-            for question in row['question']:
-                i = i+1
-                p1 = document.add_paragraph(str(i)+" : ")
-                p1.add_run(question)
-
+    add_headings(document, subject)
+    question_no = 0
+    for qtype in qtype_to_section:
+        no_of_questions = get_no_of_questions(questions_mapping, qtype)
+        question_no = add_questions(document, questions_mapping, qtype, question_no, no_of_questions)
     filename = "question_worksheet_" + datetime.datetime.now().__str__() + ".docx"
     filepath = os.path.join('searchapp/static/docs', filename)
     document.save(filepath)
--- searchapp/test_paper.py
+++ searchapp/test_paper.py
@@ -5,6 +5,25 @@ import random
 from searchapp.models import GeneratedCustomizedPaper, GeneratedTestAndGenericPaper
 from celery import shared_task
 
+def form_test_paper_dictionary(subject, chapters, subject_breakup):
+    test_paper_dict=[]
+    for question_type in subject_breakup:
+        final_list = []
+        total_question_no = subject_breakup[question_type][0]
+        q_type,weightage = get_type_and_weightage(question_type)
+        total_list = Questions.objects.filter(question_type=q_type,
+            question_weightage=weightage,
+            chapter__in=Chapter.objects.filter(id__in=chapters)).values_list('text',flat=True)
+        total_list = list(total_list)        
+        total_question_no = min(total_question_no,len(total_list))
+        final_list = random.sample(total_list,total_question_no)
+        row={
+            'question_type':question_type,
+            'question':final_list,
+            'attempt':subject_breakup.get(question_type,None)[1]
+        }
+        test_paper_dict.append(row)
+    return test_paper_dict
 
 @shared_task
 def generate_test_or_generic_paper(subject, chapters, subject_breakup, token, sorted_type):
@@ -18,27 +37,9 @@ def generate_test_or_generic_paper(subject, chapters, subject_breakup, token, so
         '3': [7, 5],
         '5': [2, 2]
     }
-
-
     it returns a list of dicts of form==>[{'question_type':'','question':'','attempt':''},{},{}]
     '''
-    test_paper_dict=[]
-    for question_type in subject_breakup:
-        final_list = []
-        total_question_no = subject_breakup[question_type][0]
-        q_type,weightage = get_type_and_weightage(question_type)
-        total_list = Questions.objects.filter(question_type=q_type,
-            question_weightage=weightage,
-            chapter__in=Chapter.objects.filter(id__in=chapters)).values_list('text',flat=True)
-        total_list = list(total_list)
-        total_question_no = min(total_question_no,len(total_list))
-        final_list = random.sample(total_list,total_question_no)
-        row={
-            'question_type':question_type,
-            'question':final_list,
-            'attempt':subject_breakup.get(question_type,None)[1]
-        }
-        test_paper_dict.append(row)
+    test_paper_dict = form_test_paper_dictionary(subject, chapters, subject_breakup)
     filepath = convert_to_doc(test_paper_dict, subject)
     GeneratedTestAndGenericPaper.objects.filter(token=token).update(file_path=filepath, is_ready=True)
 
@@ -56,24 +57,8 @@ def generate_customized_paper(subject, chapters, subject_breakup, data, mentor, 
     it returns a list of dicts of form==>{stud_name_1: [{'question_type':'','question':'','attempt':''},{},{}],stud_name_2: [{'question_type':'','question':'','attempt':''},{},{}]}
     '''
     final_dict = {}
-    for key in data:
-        test_paper_dict = []
-        for question_type in subject_breakup:
-            final_list = []
-            total_question_no = subject_breakup[question_type][0]
-            q_type,weightage = get_type_and_weightage(question_type)
-            total_list = Questions.objects.filter(question_type=q_type,
-                question_weightage=weightage,
-                chapter__in=Chapter.objects.filter(id__in=data[key])).values_list('text',flat=True)
-            total_list = list(total_list)
-            total_question_no = min(total_question_no,len(total_list))
-            final_list = random.sample(total_list,total_question_no)
-            row={
-                'question_type':question_type,
-                'question':final_list,
-                'attempt':subject_breakup.get(question_type,None)[1]
-            }
-            test_paper_dict.append(row)
-        final_dict.update({key: test_paper_dict})
+    for student_name in data:
+        test_paper_dict = form_test_paper_dictionary(subject, data[student_name], subject_breakup)
+        final_dict.update({student_name: test_paper_dict})
     filepath = convert_customized_to_doc(final_dict, subject)
     GeneratedCustomizedPaper.objects.filter(mentor__username=mentor, token=token).update(file_path=filepath, is_ready=True)
