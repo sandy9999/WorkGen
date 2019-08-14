0acfb19ad7759ca4c18702a2e16bf006f8def1d0
Akshay Pai <paiakshay998@gmail.com>
2019-08-14 10:02:18 +0530

    Convert to https

--- workgen/settings.py
+++ workgen/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'workgen.context_processors',
+                'workgen.context_processors.export_vars',
             ],
         },
     },
